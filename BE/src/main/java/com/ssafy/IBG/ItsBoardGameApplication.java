177367bc0f754c3677826e43cdae66fc5a474f6e
곽현준 <kwak0568@naver.com>
2022-03-17 11:16:56 +0900

    feat<BE, FE> : IBG First Commit

---
+++ BE/src/main/java/com/ssafy/IBG/ItsBoardGameApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.IBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsBoardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsBoardGameApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssafy/IBG/ItsBoardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.IBG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsBoardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
