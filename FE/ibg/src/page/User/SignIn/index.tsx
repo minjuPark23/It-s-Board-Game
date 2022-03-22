a1d56906bba8b7c639c9226adb524790622c8b53
leen6225 <leen6225@gmail.com>
2022-03-22 22:40:56 +0900

    feat<FE> :로그인 컨테이너 생성
    Former-commit-id: e6aee7cc99e0bb1fdf01cc149b41e0a1758954ea

--- FE/ibg/src/page/User/SignIn/index.tsx
+++ FE/ibg/src/page/User/SignIn/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "./component/Form";
+import { Grid, Box } from "@mui/material/";
+import WelcomeStepper from "../component/WelcomeStepper";
+//index에서 api 호출 -> Form에서 index(parent)로 전달
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [width] = useState(window.innerWidth);
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={2}>
+          <Form />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
