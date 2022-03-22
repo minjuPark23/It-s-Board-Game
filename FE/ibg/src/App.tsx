0fe382276dd49c70dfac4c9ba1b49284695f7d85
leen6225 <leen6225@gmail.com>
2022-03-22 14:10:15 +0900

    style<FE> : 프로젝트 컬러 테마 적용 버튼 : primary 하트 : error 테마색 : warning 으로 팔레트를 오버라이딩

--- FE/ibg/src/App.tsx
+++ FE/ibg/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Router from "./Router";
+import { CustomTheme } from "./component/CustomTheme";
+
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
-  return <Router />;
+  return (
+    <ThemeProvider theme={CustomTheme}>
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
