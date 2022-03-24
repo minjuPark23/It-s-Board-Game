42dc7afc6f266677d7704ec8e187e577da2e1418
leen6225 <leen6225@gmail.com>
2022-03-24 19:32:39 +0900

    chore<FE> : 회원가입 완료 페이지  라우터 추가

--- FE/ibg/src/Router.tsx
+++ FE/ibg/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./page/Main/index";
 import SignIn from "./page/User/SignIn/index";
 import SignUp from "./page/User/SignUp/index";
+import Complete from "./page/User/Complete/index";
 import BoardGameSearch from "./page/BoardGame/BoardGameSearch/index";
 
 function Router() {
@@ -11,6 +12,7 @@ function Router() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/search" element={<BoardGameSearch />} />
+      <Route path="/complete" element={<Complete />} />
     </Routes>
   );
 }
