c0fbf6cc0fb70d896ded3feb2b3da0a60a944c3d
kwonohbeom <xpzmfhf@gmail.com>
2022-03-24 11:58:01 +0900

    feat<BE>: 유저 API 구현

---
+++ BE/src/main/java/com/ssafy/IBG/api/UserApiController.java
@@ -0,0 +1,134 @@
+package com.ssafy.IBG.api;
+
+import com.ssafy.IBG.api.user.*;
+import com.ssafy.IBG.domain.Interest;
+import com.ssafy.IBG.api.dto.Result;
+import com.ssafy.IBG.domain.User;
+import com.ssafy.IBG.service.InterestService;
+import com.ssafy.IBG.service.ScoreService;
+import com.ssafy.IBG.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+    private final InterestService interestService;
+    private final ScoreService scoreService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저 회원 가입
+     **/
+    @PostMapping("/join")
+    public Result join(@RequestBody UserJoinRequest request){
+
+        User user = new User();
+        String encPwd = bCryptPasswordEncoder.encode(request.getUserPwd());
+
+        user.setUserEmail(request.getUserEmail());
+        user.setUserNick(request.getUserNick());
+        user.setUserPwd(encPwd);
+
+        if(userService.join(user) != null){
+            return new Result(true, HttpStatus.OK.value());
+        }else{
+            return new Result(true, HttpStatus.CONFLICT.value());
+        }
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 회원가입 시 이메일 중복 체크
+     **/
+    @PostMapping("/email")
+    public Result emailVerify(@RequestBody UserConfirmRequest request){
+
+        if(userService.confirmEmail(request.getUserEmail()))
+            return new Result(true, HttpStatus.OK.value());
+
+        return new Result(false, HttpStatus.CONFLICT.value());
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 회원가입 시 닉네임 중복 체크
+     **/
+    @PostMapping("/nickname")
+    public Result nickVerify(@RequestBody UserConfirmRequest request){
+        if(userService.confirmNick(request.getUserNick()))
+            return new Result(true, HttpStatus.OK.value());
+
+        return new Result(false, HttpStatus.CONFLICT.value());
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 회원번호로 회원 정보 조회하기
+     **/
+    @GetMapping("/user/account/{userNo}")
+    public Result getUserInfo(@PathVariable("userNo") Integer userNo){
+        User user = userService.getUser(userNo);
+
+        if(user == null)
+            return new Result(false, HttpStatus.CONFLICT.value());
+
+        UserInfoResponse response = new UserInfoResponse(user.getUserNo(), user.getUserEmail(), user.getUserNick());
+        return new Result(true, HttpStatus.OK.value(), response);
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저의 게임별 좋아요 등록 및 해제
+     **/
+    @PostMapping("/user/like")
+    public Result setInterestTransaction(@RequestBody UserInterestRequest request){
+
+        if(interestService.setInterestTransaction(request.getUserNo(), request.getGameNo()))
+            return new Result(true, HttpStatus.OK.value());
+
+        return new Result(false, HttpStatus.CONFLICT.value());
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저가 좋아요한 목록 가져오기
+     **/
+    @GetMapping("/user/like/{userNo}")
+    public Result getInterestListByUserNo(@PathVariable("userNo") Integer userNo){
+        List<Interest> list = interestService.getInterestList(userNo);
+
+        if(list.isEmpty())
+            return new Result(false, HttpStatus.NO_CONTENT.value());
+
+        return new Result(true, HttpStatus.OK.value(), list);
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저의 게임별 별점 등록 및 수정
+     **/
+    @PostMapping("/user/score")
+    public Result setScore(@RequestBody UserScoreRequest request){
+        if(!scoreService.registScore(request.getUserNo(), request.getGameNo(), request.getScoreRating()))
+            return new Result(false, HttpStatus.CONFLICT.value());
+
+        return new Result(true, HttpStatus.OK.value());
+    }
+
+
+}
---
+++ BE/src/main/java/com/ssafy/IBG/service/UserService.java
@@ -0,0 +1,54 @@
+package com.ssafy.IBG.service;
+
+import com.ssafy.IBG.domain.User;
+import com.ssafy.IBG.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저 회원 가입 후 회원 번호를 반환
+     **/
+    public Integer join(User user){
+        userRepository.save(user);
+        return user.getUserNo();
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저 회원가입 시 이메일 중복 확인 후 T/F 반환
+     **/
+    public boolean confirmEmail(String userEmail){
+        return userRepository.emailVerify(userEmail) == 0L;
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저 회원가입 시 닉네임 중복 확인 후 T/F 반환
+     **/
+    public boolean confirmNick(String userNick){
+        return userRepository.nickVerify(userNick) == 0L;
+    }
+
+    /**
+     * @author : 권오범
+     * @date : 2022-03-23
+     * @desc: 유저 회원 번호로 해당 유저 정보를 반환
+     **/
+    public User getUser(Integer userNo){
+        return userRepository.findUserByUserNo(userNo);
+    }
+
+
+}
