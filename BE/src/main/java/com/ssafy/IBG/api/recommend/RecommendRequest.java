975b620a6c7bd3ffbd4c52705c90d654e6c86af3
kwonohbeom <xpzmfhf@gmail.com>
2022-03-27 22:47:44 +0900

    style<BE>: Response DTO 수정
    Former-commit-id: 3d0e1cfec3b252531d02c0a8893983c507445bb2

--- BE/src/main/java/com/ssafy/IBG/api/recommend/RecommendRequest.java
+++ BE/src/main/java/com/ssafy/IBG/api/recommend/RecommendRequest.java
@@ -1,10 +1,8 @@
 package com.ssafy.IBG.api.recommend;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class RecommendRequest {
     private Integer userNo;
 
