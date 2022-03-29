4cb3975787af28d20053ef7b5c6d62f102fea15e
leen6225 <leen6225@gmail.com>
2022-03-29 10:45:54 +0900

    feat<FE> : 회원가입 api 연결

---
+++ FE/ibg/src/api/user.ts
@@ -0,0 +1,33 @@
+import { apiInstance, loginApiInstance } from "./index.js";
+
+const api = apiInstance();
+
+// 회원 가입
+async function join(email: string, nickname: string, password: string) {
+  return await api.post(`/join`, {
+    userEmail: email,
+    userNick: nickname,
+    userPwd: password,
+  });
+}
+
+//로그인
+async function login(email: string, password: string) {
+  return await api.post(`/login`, { userEmail: email, userPwd: password });
+}
+
+// 회원 정보
+async function userInfo(userNo: number) {
+  return (await api.get(`/user/account/${userNo}`)).data.data; //유저 객체라 data 썼는데 일단 자료가 들어와바야 알겠다.
+}
+
+//이메일 중복 조회
+async function checkEmail(email: string) {
+  return await api.post(`/email`, { userEmail: email });
+}
+//이메일 중복 조회
+async function checkNickname(nickname: string) {
+  return await api.post(`/nickname`, { userNick: nickname });
+}
+
+export { join, login, userInfo, checkEmail, checkNickname };
