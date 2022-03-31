91a4225077da0e6c1be4573f8c7f2dab4e328a5e
신미래 <sml6209@naver.com>
2022-03-31 20:53:19 +0900

    feat<FE>: 리뷰 등록 API 연결

--- FE/ibg/src/page/BoardGame/BoardGameDetail/component/ReviewInfo.tsx
+++ FE/ibg/src/page/BoardGame/BoardGameDetail/component/ReviewInfo.tsx
@@ -1,13 +1,34 @@
-import { Typography, TextField, Box, Button } from "@mui/material";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { addReview } from "../../../../api/review";
 import { Review } from "..";
 import ReviewItem from "./ReviewItem";
+import { Typography, TextField, Box, Button } from "@mui/material";
 
-export default function Reviews(props: { reviewList: Review[] }) {
+export default function Reviews(props: {
+  reviewList: Review[];
+  gameNo: number;
+  userNo: number;
+  addCallback: Function;
+}) {
   const [newReview, setNewReview] = useState("");
+  const navigate = useNavigate();
 
   const onChangeNewReview = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewReview(e.target.value);
+  };
+
+  const registerReview = () => {
+    addReview(props.gameNo, props.userNo, newReview).then((data) => {
+      if (data.code === 200) {
+        props.addCallback();
+        setNewReview("");
+      }
+    });
+  };
+
+  const movePage = () => {
+    navigate("/signin");
   };
 
   return (
@@ -18,24 +39,48 @@ export default function Reviews(props: { reviewList: Review[] }) {
         리뷰
         <Typography component="span">({props.reviewList.length})</Typography>
       </Typography>
-      {/* 리뷰 입력 창은 로그인이 안되어있으면 로그인하라고 띄우는 것이 더 좋을 듯 함 */}
-      <TextField
-        fullWidth
-        multiline
-        maxRows={5}
-        onChange={onChangeNewReview}
-      ></TextField>
-      <Box sx={{ textAlign: "right", my: 1 }}>
-        {newReview ? (
-          <Button variant="contained" sx={{ padding: 0.3 }}>
-            등록
+
+      {props.userNo ? (
+        <>
+          <TextField
+            fullWidth
+            multiline
+            value={newReview}
+            maxRows={5}
+            onChange={onChangeNewReview}
+          ></TextField>
+          <Box sx={{ textAlign: "right", my: 1 }}>
+            {newReview ? (
+              <Button
+                variant="contained"
+                sx={{ padding: 0.3 }}
+                onClick={registerReview}
+              >
+                등록
+              </Button>
+            ) : (
+              <Button
+                disabled
+                variant="contained"
+                sx={{ padding: 0.3 }}
+                onClick={registerReview}
+              >
+                등록
+              </Button>
+            )}
+          </Box>
+        </>
+      ) : (
+        <Box textAlign="center" sx={{ mb: 1 }}>
+          <Typography sx={{ fontSize: { xs: 15, md: 20 } }}>
+            로그인하면 리뷰를 등록할 수 있어요 :)
+          </Typography>
+          <Button variant="contained" sx={{ mt: 1 }} onClick={movePage}>
+            로그인하러 가기
           </Button>
-        ) : (
-          <Button disabled variant="contained" sx={{ padding: 0.3 }}>
-            등록
-          </Button>
-        )}
-      </Box>
+        </Box>
+      )}
+
       {props.reviewList.map((review) => {
         return <ReviewItem key={review.reviewNo} review={review} />;
       })}
