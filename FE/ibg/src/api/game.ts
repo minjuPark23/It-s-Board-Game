82f3e63ffc8dadd617369395a011de1aec62774d
신미래 <sml6209@naver.com>
2022-03-31 20:53:19 +0900

    feat<FE>: 리뷰 등록 API 연결
    Former-commit-id: 34053bd7da8069e41c65f2be2315201c9448a695

--- FE/ibg/src/api/game.ts
+++ FE/ibg/src/api/game.ts
@@ -2,6 +2,7 @@ import { apiInstance } from ".";
 
 const api = apiInstance();
 
+// 게임 상세정보 조회
 async function getGameDetail(gameNo: number, userNo: number) {
   return (await api.get(`/search/${gameNo}/${userNo}`)).data.data;
 }
