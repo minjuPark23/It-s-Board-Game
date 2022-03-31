a7a987c351aa969a9ea518c8d7040013739bb65f
leen6225 <leen6225@gmail.com>
2022-03-31 16:13:03 +0900

    feat<FE> : 설문조사 페이지 구성 완료

--- FE/ibg/src/page/User/Survey/index.tsx
+++ FE/ibg/src/page/User/Survey/index.tsx
@@ -1,19 +1,50 @@
 import React, { useEffect, useState } from "react";
+//컴포넌트
 import BoardCardMain from "./component/BoardCardSurvey";
-import { Button, Box, Grid, Container } from "@mui/material";
+import { Button, Box, Grid, Container, Typography } from "@mui/material";
 import WelcomeStepper from "../component/WelcomeStepper";
-import { useNavigate } from "react-router-dom";
+import ConfirmDialog from "./component/ConfirmDialog";
+//api 연결
 import { initSurvey } from "../../../api/user";
-// Game 객체 => types파일로 빼는 것이 좋음
+//네비게이션
+import { useNavigate } from "react-router-dom";
+
+// Game 객체
 export interface Game {
   gameNo: number;
   gameImg: string;
   gameName: string;
 }
+// Rated  객체
+export interface IProps {
+  ratedGameNo: number;
+  score: number;
+}
 
 export default function Survey() {
   const [gameList, setGameList] = useState<Game[]>([]);
+  const [count, setCount] = useState(0);
+  const [ratedGame, setRatedGame] = useState<number[]>([]);
   const [width] = useState(window.innerWidth);
+  const [open, setOpen] = useState(false); //modal
+  const navigate = useNavigate();
+
+  // function to handle modal open
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // function to handle modal close
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  //navigate when "Yes" is pressed on dialog OR pressed Complete
+  const handleSubmit = () => {
+    setOpen(false);
+    navigate("/complete");
+  };
+
   useEffect(() => {
     // API 연결(게임리스트 불러오기)
     setGameList(tempData.gameList);
@@ -24,6 +55,30 @@ export default function Survey() {
     // };
     // init();
   }, []);
+
+  // count 업데이트하는 method
+  const countHandler = (ratedGameNo: number, score: number) => {
+    if (score > 0) {
+      if (ratedGame.length === 0) {
+        ratedGame.push(ratedGameNo);
+        setCount(count + 1);
+      } else {
+        let found = ratedGame.includes(ratedGameNo) ? true : false;
+        if (found) {
+        } //do nothing
+        else {
+          setCount(count + 1);
+          ratedGame.push(ratedGameNo);
+        }
+      }
+    } else if (score === 0) {
+      setCount(count - 1);
+      var index = ratedGame.indexOf(ratedGameNo);
+      if (index !== -1) {
+        ratedGame.splice(index, 1);
+      }
+    }
+  };
 
   return (
     <>
@@ -38,6 +93,7 @@ export default function Survey() {
           <WelcomeStepper value="1" />
         </Box>
       </Grid>
+
       <Grid
         container
         spacing={0}
@@ -48,23 +104,37 @@ export default function Survey() {
         <Container style={{ marginTop: 20, padding: 10 }}>
           <Grid container spacing={2}>
             {gameList.map((game) => (
-              <BoardCardMain key={game.gameNo} game={game}></BoardCardMain>
+              <BoardCardMain
+                key={game.gameNo}
+                game={game}
+                parentCallback={countHandler}
+              ></BoardCardMain>
             ))}
           </Grid>
+          <Box sx={{ mb: 15 }} />
         </Container>
         <Button
-          style={{ position: "fixed", bottom: "0px" }}
+          style={{ position: "fixed", bottom: "0px", borderRadius: 0 }}
           type="submit"
           fullWidth
           variant="contained"
           color="primary"
-          size="large"
-
-          //onClick={sendData}
-          // className={classes.submit}
+          size="small"
+          onClick={count < 10 ? handleOpen : handleSubmit}
         >
-          완료
+          <Grid>
+            <Typography variant="subtitle1">완료({count})</Typography>
+            <Typography variant="caption">
+              10개 이상 평점을 등록 시 개인 맞춤 추천이 가능합니다
+            </Typography>
+          </Grid>
         </Button>
+        <ConfirmDialog
+          open={open}
+          count={count}
+          handleClose={handleClose}
+          handleSubmit={handleSubmit}
+        />
       </Grid>
     </>
   );
@@ -86,52 +156,16 @@ const tempData = {
       gameName: "Dragonmaster",
     },
     {
-      gameNo: 11,
+      gameNo: 2,
       gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
+        "https://cf.geekdo-images.com/original/img/o07K8ZVh0PkOpOnSZs1TuABb7I4=/0x0/pic4001505.jpg",
+      gameName: "Dragonmaster",
     },
     {
-      gameNo: 10,
+      gameNo: 3,
       gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 9,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 8,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 7,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 6,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 5,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
-    },
-    {
-      gameNo: 4,
-      gameImg:
-        "https://cf.geekdo-images.com/original/img/mPS50ts53753q5-kb5vWbTDN8Z0=/0x0/pic3211873.jpg",
-      gameName: "Samurai",
+        "https://cf.geekdo-images.com/original/img/o07K8ZVh0PkOpOnSZs1TuABb7I4=/0x0/pic4001505.jpg",
+      gameName: "Dragonmaster",
     },
   ],
 };
