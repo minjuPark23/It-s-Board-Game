c753959e7db9ee75ff9d7206b3db27ea916da0d7
leen6225 <leen6225@gmail.com>
2022-04-01 21:14:05 +0900

    feat<FE> : 보드게임 거래 상세정보 구조

---
+++ FE/ibg/src/page/BoardGame/BoardGameMarket/DealDetail/component/User.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import {
+  Grid,
+  Box,
+  Container,
+  Divider,
+  Avatar,
+  Button,
+  Typography,
+} from "@mui/material";
+
+interface Deal {
+  dealer: string;
+}
+
+function randomColor() {
+  let hex = Math.floor(Math.random() * 0xffffff);
+  let color = "#" + hex.toString(16);
+
+  return color;
+}
+
+export default function User({ dealer }: Deal) {
+  return (
+    <Grid direction="column">
+      <Avatar
+        style={{
+          backgroundColor: randomColor(),
+        }}
+      >
+        {dealer.charAt(0)}
+      </Avatar>
+      <Typography>{dealer}</Typography>
+    </Grid>
+  );
+}
