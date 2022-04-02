6965623e1ccd34f301cef3b5c1d45f57a55442f9
신미래 <sml6209@naver.com>
2022-04-02 19:02:47 +0900

    feat<FE>: 내비게이션 바 자동완성 및 상세 페이지로 이동

--- FE/ibg/src/api/game.ts
+++ FE/ibg/src/api/game.ts
@@ -8,7 +8,7 @@ async function getGameDetail(gameNo: number, userNo: number) {
   return (await api.get(`/search/${gameNo}/${userNo}`)).data.data;
 }
 
-// 게임 검색
+// 게임 이름으로 검색
 async function SearchByName(searchName: string, userNo: number) {
   return (
     await api.post(`/search/auto`, {
@@ -23,4 +23,9 @@ async function searchByFilter(data: ISearchFilter) {
   return (await api.post(`/search/filter`, data)).data;
 }
 
-export { getGameDetail, SearchByName, searchByFilter };
+// 전체 게임 목록 조회(자동완성용)
+async function getAutoAllGame() {
+  return (await api.get(`/search/auto`)).data;
+}
+
+export { getGameDetail, SearchByName, searchByFilter, getAutoAllGame };
