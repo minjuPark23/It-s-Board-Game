63194be320cd3b32efe6582f233d6568950e187f
kwonohbeom <xpzmfhf@gmail.com>
2022-04-03 19:05:05 +0900

    fixed<FE>: rebase 중 충돌 해결
    Former-commit-id: 2ee180b608130f2c17366a824b970902d682e77f

--- FE/ibg/src/Router.tsx
+++ FE/ibg/src/Router.tsx
@@ -3,7 +3,9 @@ import Main from "./page/Main/index";
 import SignIn from "./page/User/SignIn/index";
 import SignUp from "./page/User/SignUp/index";
 import Complete from "./page/User/Complete/index";
+import BoardGameDetail from "./page/BoardGame/BoardGameDetail";
 import BoardGameSearch from "./page/BoardGame/BoardGameSearch/index";
+import BoardGameMarket from "./page/BoardGame/BoardGameMarket/index";
 
 function Router() {
   return (
@@ -11,8 +13,10 @@ function Router() {
       <Route path="/" element={<Main />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/detail" element={<BoardGameDetail />} />
       <Route path="/search" element={<BoardGameSearch />} />
       <Route path="/complete" element={<Complete />} />
+      <Route path="/market" element={<BoardGameMarket />} />
     </Routes>
   );
 }
