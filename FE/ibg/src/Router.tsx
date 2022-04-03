095ce9631c564894abdd93af3f42bc7bf6e9ac32
kwonohbeom <xpzmfhf@gmail.com>
2022-04-03 19:03:09 +0900

    fixed<FE>: rebase 중 충돌 해결
    Former-commit-id: a9327ff115b84754eab52a575c0e3251be59401e

--- FE/ibg/src/Router.tsx
+++ FE/ibg/src/Router.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import Main from "./page/Main/index";
-import BoardGameSearch from "./page/BoardGameSearch/index";
+// import Main from "./page/main/index";
+import SignIn from "./page/User/SignIn/index";
+import SignUp from "./page/User/SignUp/index";
+import BoardGameSearch from "./page/BoardGame/BoardGameSearch/index";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      {/* <Route path="/" element={<Main />} /> */}
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/search" element={<BoardGameSearch />} />
     </Routes>
   );
