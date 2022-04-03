11b72d4bd38e2fc041d65bc9eeae8b3536eb589a
신미래 <sml6209@naver.com>
2022-04-03 20:18:00 +0900

    chore<FE>: 메인 스켈레톤 수정
    - 테마별로 스켈레톤 적용

--- FE/ibg/src/page/Main/index.tsx
+++ FE/ibg/src/page/Main/index.tsx
@@ -32,7 +32,7 @@ export interface Game {
 // 테마별 게임리스트: sm(600) 이상(pc)에서는 버튼으로, 이하(모바일)에서는 스크롤로 동작
 export default function Main() {
   const userNo = useSelector((state: RootStateOrAny) => state.user.userNo);
-  const [loading, setLoading] = useState(true);
+
   const [userGameList, setUserGameList] = useState<Game[]>([]);
   const [categoryGameList, setCategoryGameList] = useState<Game[]>([]);
   const [weightGameList, setWeightGameList] = useState<Game[]>([]);
@@ -43,64 +43,74 @@ export default function Main() {
   const [reviewGameList, setReviewGameList] = useState<Game[]>([]);
   const [scoreGameList, setScoreGameList] = useState<Game[]>([]);
 
+  const [userLoading, setUserLoading] = useState<boolean>(userNo);
+  const [categoryLoading, setCategoryLoading] = useState<boolean>(userNo);
+  const [weightLoading, setWeightLoading] = useState<boolean>(userNo);
+  const [playerLoading, setPlayerLoading] = useState<boolean>(userNo);
+  const [timeLoading, setTimeLoading] = useState<boolean>(userNo);
+  const [ageLoading, setAgeLoading] = useState<boolean>(userNo);
+  const [newbieLoading, setNewbieLoading] = useState<boolean>(true);
+  const [reviewLoading, setReviewLoading] = useState<boolean>(true);
+  const [scoreLoading, setScoreLoading] = useState<boolean>(true);
+
   useEffect(() => {
     // 로그인 한 경우
     if (userNo) {
       getRecommByUser(userNo).then((data) => {
         if (data.code === 200) {
           setUserGameList(data.data);
-          setLoading(false);
         }
+        setUserLoading(false);
       });
       getRecommByCategory().then((data) => {
         if (data.code === 200) {
           setCategoryGameList(data.data);
-          setLoading(false);
         }
+        setCategoryLoading(false);
       });
       getRecommByWeight().then((data) => {
         if (data.code === 200) {
           setWeightGameList(data.data);
-          setLoading(false);
         }
+        setWeightLoading(false);
       });
       getRecommByPlayer().then((data) => {
         if (data.code === 200) {
           setPlayerGameList(data.data);
-          setLoading(false);
         }
+        setPlayerLoading(false);
       });
       getRecommByTime().then((data) => {
         if (data.code === 200) {
           setTimeGameList(data.data);
-          setLoading(false);
         }
+        setTimeLoading(false);
       });
       getRecommByAge().then((data) => {
         if (data.code === 200) {
           setAgeGameList(data.data);
-          setLoading(false);
         }
+        setAgeLoading(false);
       });
     }
     // 공통
     getRecommByNewbie().then((data) => {
       if (data.code === 200) {
         setNewbieGameList(data.data);
-        setLoading(false);
       }
+      setNewbieLoading(false);
     });
     getRecommByReviews().then((data) => {
       if (data.code === 200) {
         setReviewGameList(data.data);
-        setLoading(false);
       }
+      setReviewLoading(false);
     });
     getRecommByScore().then((data) => {
       if (data.code === 200) {
         setScoreGameList(data.data);
-        setLoading(false);
       }
+      setScoreLoading(false);
     });
   }, [userNo]);
 
@@ -133,53 +143,89 @@ export default function Main() {
 
   return (
     <Container style={{ padding: 20 }}>
-      {loading && (
-        <>
-          <SkelTheme />
-          <SkelTheme />
-        </>
+      {userLoading ? (
+        <SkelTheme />
+      ) : (
+        userGameList.length > 0 && (
+          <ThemeList title="나의 맞춤 추천 게임" gameList={userGameList} />
+        )
       )}
-      {userGameList.length > 0 && (
-        <ThemeList title="나의 맞춤 추천 게임" gameList={userGameList} />
+      {categoryLoading ? (
+        <SkelTheme />
+      ) : (
+        categoryGameList.length > 0 && (
+          <ThemeList
+            title="내가 좋아하는 카테고리의 게임"
+            gameList={categoryGameList}
+          />
+        )
       )}
-      {categoryGameList.length > 0 && (
-        <ThemeList
-          title="내가 좋아하는 카테고리의 게임"
-          gameList={categoryGameList}
-        />
+      {weightLoading ? (
+        <SkelTheme />
+      ) : (
+        weightGameList.length > 0 && (
+          <ThemeList
+            title="내가 좋아하는 난이도의 게임"
+            gameList={weightGameList}
+          />
+        )
       )}
-      {weightGameList.length > 0 && (
-        <ThemeList
-          title="내가 좋아하는 난이도의 게임"
-          gameList={weightGameList}
-        />
+
+      {playerLoading ? (
+        <SkelTheme />
+      ) : (
+        playerGameList.length > 0 && (
+          <ThemeList
+            title="내가 즐겨하는 인원 수의 게임"
+            gameList={playerGameList}
+          />
+        )
       )}
-      {playerGameList.length > 0 && (
-        <ThemeList
-          title="내가 즐겨하는 인원 수의 게임"
-          gameList={playerGameList}
-        />
+
+      {timeLoading ? (
+        <SkelTheme />
+      ) : (
+        timeGameList.length > 0 && (
+          <ThemeList
+            title="내가 즐겨하는 플레이 시간의 게임"
+            gameList={timeGameList}
+          />
+        )
       )}
-      {timeGameList.length > 0 && (
-        <ThemeList
-          title="내가 즐겨하는 플레이 시간의 게임"
-          gameList={timeGameList}
-        />
+
+      {ageLoading ? (
+        <SkelTheme />
+      ) : (
+        ageGameList.length > 0 && (
+          <ThemeList
+            title="내가 즐겨하는 나이대의 게임"
+            gameList={ageGameList}
+          />
+        )
       )}
-      {ageGameList.length > 0 && (
-        <ThemeList title="내가 즐겨하는 나이대의 게임" gameList={ageGameList} />
+      {newbieLoading ? (
+        <SkelTheme />
+      ) : (
+        newbieGameList.length > 0 && (
+          <ThemeList
+            title="초보자라면 이 게임 어때요?"
+            gameList={newbieGameList}
+          />
+        )
       )}
-      {newbieGameList.length > 0 && (
-        <ThemeList
-          title="초보자라면 이 게임 어때요?"
-          gameList={newbieGameList}
-        />
+      {scoreLoading ? (
+        <SkelTheme />
+      ) : (
+        scoreGameList.length > 0 && (
+          <ThemeList title="이보게 평점이 높은 게임" gameList={scoreGameList} />
+        )
       )}
-      {scoreGameList.length > 0 && (
-        <ThemeList title="이보게 평점이 높은 게임" gameList={scoreGameList} />
-      )}
-      {reviewGameList.length > 0 && (
-        <ThemeList title="리뷰가 많은 게임" gameList={reviewGameList} />
+      {reviewLoading ? (
+        <SkelTheme />
+      ) : (
+        reviewGameList.length > 0 && (
+          <ThemeList title="리뷰가 많은 게임" gameList={reviewGameList} />
+        )
       )}
     </Container>
   );
