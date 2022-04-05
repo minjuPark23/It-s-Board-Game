3fe1cb5dd56866aa3c014ff078e36a07246a1446
leen6225 <leen6225@gmail.com>
2022-04-05 15:17:39 +0900

    style<FE> : 네비바 스타일 변경

--- FE/ibg/src/component/layout/NavBar.tsx
+++ FE/ibg/src/component/layout/NavBar.tsx
@@ -33,6 +33,7 @@ import Logout from "@mui/icons-material/Logout";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import MapIcon from "@mui/icons-material/Map";
+import { Typography } from "@mui/material";
 
 // Nav 항목 - link가 존재하면 페이지 이동, method가 존재하면 해당 함수 실행(handleNavMethod 추가 필요)
 const pages = [
@@ -54,10 +55,13 @@ const userNav = [
   { label: "로그아웃", icon: <Logout />, method: "logout" },
 ];
 
-const StyledAppBar = styled(AppBar)(() => ({
-  position: "static",
-  backgroundColor: "transparent",
-  boxShadow: "rgba(33, 35, 38, 0.1) 0px 10px 10px -10px",
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  position: "fixed",
+  //backgroundColor: "error",
+  backgroundColor: theme.palette.warning.main,
+  //boxShadow: "rgba(33, 35, 38, 0.1) 0px 10px 10px -10px",
+  boxShadow: "rgba(33, 35, 38, 0.1) 0px 0px 0px 0px",
+  //  maxHeight: 50,
 }));
 
 export default function NavBar() {
@@ -182,14 +186,28 @@ export default function NavBar() {
         <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
           <ImageListItem
             sx={{
-              maxWidth: 50,
+              maxWidth: 40,
               cursor: "pointer",
+              //  mb: { md: 2, xs: 1 },
+              ml: { md: 15 },
             }}
             onClick={() => movePage("/")}
           >
             <img src="img/logo.png" alt="logo" />
           </ImageListItem>
-
+          <Typography
+            sx={{
+              mr: { xs: 0, md: 3 },
+              minWidth: 65,
+              cursor: "pointer",
+              display: { xs: "none", md: "block" },
+              // mb: 2,
+              ml: { md: 2 },
+            }}
+          >
+            이보게
+          </Typography>
+          {/*
           <ImageListItem
             sx={{
               mr: { xs: 0, md: 3 },
@@ -201,6 +219,7 @@ export default function NavBar() {
           >
             <img src="img/logotext.PNG" alt="logoText" />
           </ImageListItem>
+            */}
 
           {/* Nav 반응형 - PC --------------------------------------*/}
           {/* 페이지 이동 Nav */}
@@ -208,7 +227,11 @@ export default function NavBar() {
             {pages.map((page) => (
               <Button
                 key={page.label}
-                sx={{ my: 2, color: "black", display: "block" }}
+                sx={{
+                  color: "white",
+                  display: "block",
+                  // mb: 2
+                }}
                 onClick={() => movePage(page.link)}
               >
                 {page.label}
@@ -223,7 +246,12 @@ export default function NavBar() {
           />
           {/* 오른쪽 메뉴(사용자) auth: 로그인 여부 */}
           {auth ? (
-            <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
               <Tooltip title="사용자 메뉴">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <AvatarGenerator userName={userName} isNav={true} />
@@ -291,7 +319,12 @@ export default function NavBar() {
             <Box sx={{ flexGrow: 0 }}>
               <Button
                 color="warning"
-                sx={{ color: "black", display: { xs: "none", md: "block" } }}
+                sx={{
+                  color: "white",
+                  display: { xs: "none", md: "block" },
+                 // mb: { md: 2 },
+                  mr: { md: 15 },
+                }}
                 onClick={() => movePage("/signin")}
               >
                 로그인
