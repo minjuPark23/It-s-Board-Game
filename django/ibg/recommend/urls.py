2a89d56e8491cc7f50a59e323a24d6d3db753800
mindu23 <mindj5541@google.com>
2022-04-06 11:34:03 +0900

    feat<BE> - linear regression Lasso를 사용한 사용자 평점 예측
    Former-commit-id: a50c1b7289cd263c8dd49a6902802b2d606deb5c

--- django/ibg/recommend/urls.py
+++ django/ibg/recommend/urls.py
@@ -6,7 +6,8 @@ from .views import UserView
 urlpatterns = format_suffix_patterns([
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path("predict/user/<int:user_no>/",  UserView.recommend_by_predicted_score_for_knn),
-    path("user/predict2/<int:user_no>/", UserView.recommend_by_predicted_score_for_svd),
+    path("predict/user2/<int:user_no>/", UserView.recommend_by_predicted_score_for_svd),
+    path("predict/user3/<int:user_no>/", UserView.recommend_by_predicted_score_for_lasso),
     path("predict/popular", UserView.recommend_by_score_on_score_count),
     path("predict/desc/<int:game_no>/", UserView.recommend_by_desc_similartiy),
     path("predict/category/<int:game_no>", UserView.recommend_by_category_similartiy)
