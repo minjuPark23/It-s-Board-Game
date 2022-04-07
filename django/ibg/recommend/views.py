1ef994e1f546b59d63b04edfdc0dc00aa7dc276d
곽현준 <kwak0568@naver.com>
2022-04-07 23:33:33 +0900

    chore<BE>: django 테스트
    Former-commit-id: 8adf14e3ff43170709adf76a95a293e8eea6bf5e

--- django/ibg/recommend/views.py
+++ django/ibg/recommend/views.py
@@ -129,11 +129,10 @@ class UserView(viewsets.ModelViewSet):
         categorys = game_list['game_category'].str.get_dummies("|")
 
         scores = Score.objects.filter(user_no=user_no)
-        user_score_list = pd.DataFrame(scores.values("score_no", "game_no", "user_no", "score_rating")).set_index(
-            'score_no')
+        user_score_list = pd.DataFrame(scores.values("score_no", "game_no", "user_no", "score_rating")).set_index('score_no')
         user_score_list = user_score_list.merge(categorys, left_on='game_no', right_index=True)
 
-        model = Lasso(alpha=0.5)
+        model = Lasso(alpha=0.003)
         X = user_score_list[categorys.columns]
         y = user_score_list['score_rating']
         model.fit(X, y)
