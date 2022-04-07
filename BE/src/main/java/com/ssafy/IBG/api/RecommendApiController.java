da263fca27bf63a4cd2fbcf6c5d49df14d2412c4
mindu23 <mindj5541@google.com>
2022-04-08 03:34:06 +0900

    chore<BE>: - 오류 수정 recommend table 미사용
    Former-commit-id: ac590d135670b6120b0ff83c27c24d2a518d8a6d

--- BE/src/main/java/com/ssafy/IBG/api/RecommendApiController.java
+++ BE/src/main/java/com/ssafy/IBG/api/RecommendApiController.java
@@ -132,32 +132,32 @@ public class RecommendApiController {
      * */
     @GetMapping("/game/score/{userNo}")
     public Result getRecommendByScore(@PathVariable(name = "userNo") Integer userNo) throws JsonProcessingException {
-        int scoreCnt = scoreService.getScoreCnt(userNo);
-        if (scoreCnt < 10) {
-            System.out.println("아직 평점 데이터 10개가 안된다.");
-            return new Result(HttpStatus.OK.value(), null);
-        }
-        List<Recommend> recommendList = recommendService.getRecommendByUserNo(userNo);
-
-        System.out.println("평점 데이터 충분해 추천 데이터 반환. "+ recommendList.size());
-
-        List<Game> collect = recommendList.stream().map(r -> r.getGame()).collect(Collectors.toList());
-
-        Collections.shuffle(collect);
-
-        return getResultList(collect, userNo);
-
-//        List<Score> scores = scoreService.getScoreListByUserNoOrderByRating(userNo);
+//        int scoreCnt = scoreService.getScoreCnt(userNo);
+//        if (scoreCnt < 10) {
+//            System.out.println("아직 평점 데이터 10개가 안된다.");
+//            return new Result(HttpStatus.OK.value(), null);
+//        }
+//        List<Recommend> recommendList = recommendService.getRecommendByUserNo(userNo);
 //
-//        if(scores.size() < 10)
-//            return new Result(HttpStatus.NO_CONTENT.value());
+//        System.out.println("평점 데이터 충분해 추천 데이터 반환. "+ recommendList.size());
 //
+//        List<Game> collect = recommendList.stream().map(r -> r.getGame()).collect(Collectors.toList());
 //
-//        List<Integer> gameNoList = restapiService.requestGETAPI("/user3", userNo);
+//        Collections.shuffle(collect);
 //
-//        List<Game> game_popular_list = gameNoList.stream().map(no -> gameService.getGameByGameNo(no)).collect(Collectors.toList());
-//
-//        return getResultList(game_popular_list, userNo);
+//        return getResultList(collect, userNo);
+
+        List<Score> scores = scoreService.getScoreListByUserNoOrderByRating(userNo);
+
+        if(scores.size() < 10)
+            return new Result(HttpStatus.NO_CONTENT.value());
+
+
+        List<Integer> gameNoList = restapiService.requestGETAPI("/user3", userNo);
+
+        List<Game> game_popular_list = gameNoList.stream().map(no -> gameService.getGameByGameNo(no)).collect(Collectors.toList());
+
+        return getResultList(game_popular_list, userNo);
     }
 
     /**
@@ -214,9 +214,7 @@ public class RecommendApiController {
         String target = gameService.getGameByGameNo(game_no).getGameKorName();
 
         List<Game> gameList = list.stream()
-                .map(no-> {
-                    return gameService.getGameByGameNo(no);
-                })
+                .map(no-> gameService.getGameByGameNo(no))
                 .collect(Collectors.toList());
 
         Collections.shuffle(gameList);
